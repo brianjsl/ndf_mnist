98b9623caf37742ef3f9ca70e10c9318708f9391
Brian Lee <brianjslmit@gmail.com>
2022-03-04 17:06:50 -0500

    small change to generator

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 This is the code for finding image correspondence on an overlapping MNIST dataset.
 
-##Generating Data
+## Generating Data
 Run dataloader.py as follows:
 ```
-python3 dataloader.py --
+python3 generator.py --overlapmnist_path ./data/MNIST/overlapMNIST --num_image_per_class 1000 --image_size 42 42 
 ```
--- generator.py
+++ generator.py
@@ -38,6 +38,70 @@ def download_mnist(data_dir):
         print('Unzip ', k)
         subprocess.call(cmd)
 
+def extract_mnist(data_dir):
+    '''
+    Returns MNIST dataset given the data directory (not seperated into test and train). 
+    Total of 70000 images.
+    :params
+    :data_dir - directory of the data
+    '''
+
+    num_mnist_train = 60000
+    num_mnist_test = 10000
+
+    fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
+    loaded = np.fromfile(file = fd, dtype = np.uint8)
+    train_image = loaded[16:].reshape((num_mnist_train, 28, 28, 1))
+
+    fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    train_label = np.asarray(loaded[8:].reshape((num_mnist_train)))
+
+    fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    test_image = loaded[16:].reshape((num_mnist_test, 28, 28, 1))
+
+    fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+    loaded = np.fromfile(file=fd, dtype=np.uint8)
+    test_label = np.asarray(loaded[8:].reshape((num_mnist_test)))
+
+    return np.concatenate((train_image, test_image)), \
+        np.concatenate((train_label, test_label))
+
+
+def sample_coordinates(high, size):
+    pass
+
+def generator(config):
+    #check if mnist is downloaded.
+    check_mnist_dir(config.mnist_path)
+
+    images, labels = extract_mnist(config.mnist_path)
+    h, w = images.shape[1:3] #shape of the images (default 28x28 if using classic MNIST)
+
+    #split the dataset. 
+    rs = np.random.RandomState(config.random_seed) #generate random state from random seed
+    num_original_class = len(np.unique(labels)) #number of unique classes (10) in the original dataset
+    num_class = len(np.unique(labels))**2 #number of classes (one for each tuple)
+    classes = list(np.array(range(num_class)))
+    rs.shuffle(classes)
+    num_train, num_val, num_test = [
+            int(float(ratio)/np.sum(config.train_val_test_ratio)*num_class)
+            for ratio in config.train_val_test_ratio]
+    train_classes = classes[:num_train]
+    val_classes = classes[num_train:num_train+num_val]
+    test_classes = classes[num_train+num_val:]
+
+    # label index
+    indexes = []
+    for c in range(num_original_class):
+        indexes.append(list(np.where(labels == c)[0]))
+
+    
+    
+
+    pass
+
 def argparser():   
     '''
     Argparser Setup. 
@@ -79,3 +143,4 @@ def argparser():
 
 if __name__ == '__main__':
     config = argparser()
+    
